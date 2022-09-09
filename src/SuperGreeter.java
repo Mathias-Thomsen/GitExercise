c90b3d479f640cdc4c0bde6772d2492841412998
Mathias-Thomsen <math831t6@stud.kea.dk>
2022-09-09 13:46:19 +0200

    added askAboutAge

--- src/SuperGreeter.java
+++ src/SuperGreeter.java
@@ -1,14 +1,14 @@
 import java.util.Scanner;
 
 public class SuperGreeter {
-
+    Scanner scanner = new Scanner(System.in);
     public void start() {
         System.out.println("Det virker!");
 
     }
 
     public void greet() {
-        Scanner scanner = new Scanner(System.in);
+
         System.out.print("Indtast dit navn: ");
         String navn = scanner.nextLine();
 
@@ -17,10 +17,18 @@ public class SuperGreeter {
 
     }
 
+    public void askAboutAge() {
+        System.out.print("Hvad er din alder: ");
+        int alder = scanner.nextInt();
+
+        System.out.println("Din alder er: " + alder);
+    }
+
     public static void main(String[] args) {
         SuperGreeter sg = new SuperGreeter();
         sg.start();
         sg.greet();
+        sg.askAboutAge();
 
 
     }
