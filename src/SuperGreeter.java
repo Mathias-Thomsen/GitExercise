3b1939c1fcc104895c81dfaccf4e00578ec453c7
Mathias-Thomsen <math831t6@stud.kea.dk>
2022-09-09 13:39:22 +0200

    added greet

--- src/SuperGreeter.java
+++ src/SuperGreeter.java
@@ -1,13 +1,26 @@
+import java.util.Scanner;
+
 public class SuperGreeter {
 
-    public static void start() {
+    public void start() {
         System.out.println("Det virker!");
+
+    }
+
+    public void greet() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Indtast dit navn: ");
+        String navn = scanner.nextLine();
+
+        System.out.println("Hej " + navn);
+
 
     }
 
     public static void main(String[] args) {
         SuperGreeter sg = new SuperGreeter();
         sg.start();
+        sg.greet();
 
 
     }
