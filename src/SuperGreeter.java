44ee01b334c47abd3a03f353ff4c72f84bc9a765
Mathias-Thomsen <math831t6@stud.kea.dk>
2022-09-09 13:35:29 +0200

    initial commit

---
+++ src/SuperGreeter.java
@@ -0,0 +1,15 @@
+public class SuperGreeter {
+
+    public static void start() {
+        System.out.println("Det virker!");
+
+    }
+
+    public static void main(String[] args) {
+        SuperGreeter sg = new SuperGreeter();
+        sg.start();
+
+
+    }
+
+}
